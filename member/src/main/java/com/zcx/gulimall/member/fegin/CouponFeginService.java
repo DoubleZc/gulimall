2e8d1ac1aa9bb6ec96072689479f62ce0b9c8198
zcx <3111782792@qq.com>
2022-06-22 15:39:40 +0800

    初始工程

---
+++ member/src/main/java/com/zcx/gulimall/member/fegin/CouponFeginService.java
@@ -0,0 +1,15 @@
+package com.zcx.gulimall.member.fegin;
+
+
+import com.zcx.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("gulimall-coupon")
+public interface CouponFeginService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R memeberCoupon() ;
+
+
+}
